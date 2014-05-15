cf5c5354e5c2f049c65535c52eed9d9aea1059be
jaeseon <shinjaeseon@hotmail.com>
2014-05-15 19:24:26 +0900

    Day07.GraphSerialize 프로젝트 추가.

---
+++ Day07.GraphSerialize.Test/GraphItemTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Day07.GraphSerialize.Test
+{
+	[TestClass]
+	public class GraphItemTests
+	{
+		[TestMethod]
+		public void DataContractWriteTest()
+		{
+			List<GraphItem> items = new List<GraphItem>();
+			for (int i = 0; i < 100000; i++)
+			{
+				var item = new GraphItem();
+				item.C1 = 1.3f;
+				item.C2 = 1.44f;
+				items.Add(item);
+			}
+
+			var result = GraphItem.DataContractSerializ(items, "d:\\testFile2.bin");
+			Assert.IsTrue(result);
+			var kk = GraphItem.DataContractDeserialize("d:\\testFile2.bin");
+
+			Assert.IsTrue((kk[0].C1 == items[0].C1));
+		}
+
+		public void DataContractReadTest()
+		{
+
+		}
+	}
+}
---
+++ Day07.GraphSerialize/GraphItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07.GraphSerialize
+{
+	[DataContract]
+	public class GraphItem
+	{
+		[DataMember]
+		public float C1 { get; set; }
+		[DataMember]
+		public float C2 { get; set; }
+		[DataMember]
+		public float VPP { get; set; }
+		[DataMember]
+		public float IPP { get; set; }
+		[DataMember]
+		public float BIAS { get; set; }
+		[DataMember]
+		public float R { get; set; }
+		[DataMember]
+		public float RX { get; set; }
+
+		public static bool DataContractSerializ(List<GraphItem> items, string filename)
+		{
+			//FileStream stream = new FileStream(@"d:\bookDataB.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+			//DataContractSerializer dcs = new DataContractSerializer(typeof(AuthorB));
+			//XmlDictionaryWriter xdw = XmlDictionaryWriter.CreateTextWriter(stream, Encoding.UTF8);
+			//dcs.WriteObject(xdw, book);
+			//stream.Close();
+
+			try
+			{
+				var serializer = new DataContractSerializer(typeof(List<GraphItem>));
+				FileStream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+				//using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+				using (var writer = new BinaryWriter(stream))
+				{
+					serializer.WriteObject(stream, items);
+				}
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static List<GraphItem> DataContractDeserialize(string filename)
+		{
+			var serializer = new DataContractSerializer(typeof(List<GraphItem>));
+			using (var stream = new FileStream(filename, FileMode.Open))
+			//using (var reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max))
+			using (var reader = new BinaryReader(stream))
+			{
+				return (List<GraphItem>)serializer.ReadObject(stream);
+			}
+		}
+	}
+}
